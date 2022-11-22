27b126724da258bed63714178879c61fb690025f
Diog9TV <89103529+Diog9TV@users.noreply.github.com>
2022-11-22 16:37:22 -0300

    Add files via upload

---
+++ assets/preload/data/faceless/script.lua
@@ -0,0 +1,10 @@
+function onStepHit()
+	if curStep == 1026 then
+      
+      makeLuaSprite('wbstage4', 'wbstage4', -400, -300);
+      scaleObject('wbstage4', 4.5, 4.5);
+
+      addLuaSprite('wbstage4', false);
+
+   end
+end
